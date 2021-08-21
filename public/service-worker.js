5006aab3b3b80adb38f88c9fc5eece760876072d
SL <samanthl795@gmail.com>
2021-08-20 21:11:30 -0700

    Log method for request

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,7 +45,7 @@ const FILES_TO_CACHE = [
   // fetch
   self.addEventListener("fetch", function(evt) {
     // cache successful requests to the API
-    console.log("Fetching " + evt.request);
+    console.log("Fetching " + evt.request.method);
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
